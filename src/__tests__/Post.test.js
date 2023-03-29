44d6da880bfc81a2ab1efd56d81e144ff34477ed
StuartCowley <voidstalker@live.co.uk>
2023-03-29 20:13:41 +0100

    assert author is correct

--- src/__tests__/Post.test.js
+++ src/__tests__/Post.test.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { render, screen } from "@testing-library/react";
 import Post from "../components/Post";
 import renderer from "react-test-renderer";
 
 describe("Post", () => {
+  const validProps = {
+    postData: {
+      title: "test title",
+      author: "test author",
+      date: "test date",
+      isPublished: true,
+      body: "test body",
+      tags: ["test tag 1", "test tag 2", "test tag 3"],
+    },
+    handleUpvote: jest.fn(),
+  };
+
   test("renders as expected", () => {
-    const validProps = {
-      postData: {
-        title: "test title",
-        author: "test author",
-        date: "test date",
-        isPublished: true,
-        body: "test body",
-        tags: ["test tag 1", "test tag 2", "test tag 3"],
-      },
-      handleUpvote: jest.fn(),
-    };
     const rendered = renderer.create(
       <Post
         postData={validProps.postData}
@@ -23,5 +24,16 @@ describe("Post", () => {
     );
 
     expect(rendered).toMatchSnapshot();
+  });
+
+  test("Assert author is present", () => {
+    render(
+      <Post
+        postData={validProps.postData}
+        handleUpvote={validProps.handleUpvote}
+      />
+    );
+
+    expect(screen.getByText("Author: test author")).toBeInTheDocument();
   });
 });
